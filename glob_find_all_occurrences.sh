2849bda765a2a94af6e43b9e7fe6a4cf5f5acadd
Matt Norman <normanmr@ornl.gov>
2018-09-11 13:11:42 -0400

    Update glob_find_all_occurrences.sh
    case insensitive and includes non-f90 files

--- glob_find_all_occurrences.sh
+++ glob_find_all_occurrences.sh
@@ -2,7 +2,7 @@
 
 echo "Finding all occurences of: $1"
 
-for f in `find ~/ACME-ECP/components/cam/src/physics/crm -name "*.F90"` ; do
+for f in `find ~/ACME-ECP/components/cam/src/physics/crm -iname "*.F*"` ; do
   #Find all occurences of unique variable name & exclude lines that are entirely comments
   grep -irnGH "[^a-zA-Z0-9_%]$1[^a-zA-Z0-9_%]" $f  # | grep -vG "$f\:[0-9]*\:[ \t]*!"
   #Also include case where variable name starts on the first column in a line
