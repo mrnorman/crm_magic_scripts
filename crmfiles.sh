6a5e27ada590824c979d04c96c27e6fd127ba9e4
Matt Norman <normanmr@ornl.gov>
2020-01-07 10:42:54 -0500

    making this work better

--- crmfiles.sh
+++ crmfiles.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-find /home/imn/ACME-ECP/components/cam/src/physics/crm -type f -iname "*.f*" | grep -v "UM5" | grep -v "MICRO_M2005/" | grep -v "SGS_CLUBBkvhkvm/" | grep -v "CLUBB/" | grep -v module_mp_graupel.F90 | grep -v crm_ecpp_output_module.F90 | grep -v crm_input_module.F90 | grep -v crm_output_module.F90 | grep -v crm_rad_module.F90 | grep -v crm_state_module.F90
-#echo /home/imn/ACME-ECP/components/cam/src/physics/cam/crm_physics.F90
+find /ccs/home/$USER/ACME-ECP/components/cam/src/physics/crm -type f -iname "*.f*" | grep -v "UM5" | grep -v "MICRO_M2005" | grep -v "SGS_CLUBBkvhkvm" | grep -v "CLUBB" | grep -v module_mp_graupel.F90 | grep -v crm_ecpp_output_module.F90 | grep -v crm_input_module.F90 | grep -v crm_output_module.F90 | grep -v crm_rad_module.F90 | grep -v crm_state_module.F90
 
-#find /home/imn/ACME-ECP/components/cam/src/physics -type f -iname "*.f*"
--- find_all_occurrences.sh
+++ find_all_occurrences.sh
@@ -4,7 +4,7 @@
 
 echo "Finding all occurences of: $1"
 
-for f in `crmfiles.sh` ; do
+for f in `./crmfiles.sh` ; do
   #Find all occurences of unique variable name & exclude lines that are entirely comments
   grep -irnGH "[^a-zA-Z0-9_%]$1[^a-zA-Z0-9_%]" $f | grep -vG "$f\:[0-9]*\:[ \t]*!"
   #grep -irnGH "[^a-zA-Z0-9_]$1[^a-zA-Z0-9_]" $f | grep -vG "$f\:[0-9]*\:[ \t]*!"
